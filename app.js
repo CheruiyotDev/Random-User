dfe91eeb75944e4b8b3f68255742f47d7dcc795c
Evans <chersoftwaredev@gmail.com>
2024-11-23 10:29:12 +0300

    Completed the display user and fetch user

--- app.js
+++ app.js
@@ -1,26 +1,8 @@
+import displayUser from './utils/displayUser.js';
 import getUser from './utils/fetchUser.js';
 import get from './utils/getElement.js';
 
-const img = get('.user-img');
-const title = get('.user-title');
-const value = get('.user-value');
 const btn = get('.btn');
-const btns = [...document.querySelectorAll('.icon')];
-console.log(btns);
-
-const displayUser = (person) => {
-  img.src = person.image;
-  value.textContent = person.name;
-  title.textContent = `My name is`;
-  btns[0].classList.add('active');
-  btns.forEach((btn) => {
-    const label = btn.dataset.label;
-    btn.addEventListener('click', () => {
-      title.textContent = `My ${label} is`;
-      value.textContent = person[label];
-    });
-  });
-};
 
 const showUser = async () => {
   const person = await getUser();
