83e58bd8e6aa144cc41f5e595aa0745355bf0c59
Evans <chersoftwaredev@gmail.com>
2024-11-23 09:20:05 +0300

    added display user

--- app.js
+++ app.js
@@ -1,13 +1,31 @@
-const URL = 'https://randomuser.me/api';
+import getUser from './utils/fetchUser.js';
+import get from './utils/getElement.js';
 
-const fetchData = async () => {
-  try {
-    const response = await fetch(URL);
-    const data = await response.json();
-    console.log(data);
-    return data;
-  } catch (error) {
-    console.log(error);
-  }
+const img = get('.user-img');
+const title = get('.user-title');
+const value = get('.user-value');
+const btn = get('.btn');
+const btns = [...document.querySelectorAll('.icon')];
+console.log(btns);
+
+const displayUser = (person) => {
+  img.src = person.image;
+  value.textContent = person.name;
+  title.textContent = `My name is`;
+  btns[0].classList.add('active');
+  btns.forEach((btn) => {
+    const label = btn.dataset.label;
+    btn.addEventListener('click', () => {
+      title.textContent = `My ${label} is`;
+      value.textContent = person[label];
+    });
+  });
 };
-fetchData();
+
+const showUser = async () => {
+  const person = await getUser();
+  displayUser(person);
+};
+
+window.addEventListener('DOMContentLoaded', showUser);
+btn.addEventListener('click', showUser);
